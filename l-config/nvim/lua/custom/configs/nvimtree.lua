49712da664517b8a8458465b81b0711e7bac2591
yoda31217 <jbourne.nik@gmail.com>
2023-10-01 19:28:26 +0300

    nvimtree - disabled highlighting of opened files

--- l-config/nvim/lua/custom/configs/nvimtree.lua
+++ l-config/nvim/lua/custom/configs/nvimtree.lua
@@ -4,10 +4,8 @@ nvimtree_config.renderer.icons.show.git = true
 nvimtree_config.renderer.icons.modified_placement = "before"
 nvimtree_config.renderer.root_folder_label = ":t"
 nvimtree_config.renderer.highlight_git = true
-nvimtree_config.renderer.highlight_opened_files = "all"
+nvimtree_config.renderer.highlight_opened_files = "none"
 nvimtree_config.renderer.highlight_modified = "all"
-nvimtree_config.modified = {
-	enable = true,
-}
+nvimtree_config.modified = { enable = true }
 nvimtree_config.git.enable = true
 nvimtree_config.git.ignore = false
