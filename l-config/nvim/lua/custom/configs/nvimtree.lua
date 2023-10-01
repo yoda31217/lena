de8ca469cd9ecea892a8f4e9ee7a98bc75209985
yoda31217 <jbourne.nik@gmail.com>
2023-10-01 18:43:23 +0300

    Modified files and git highlighting in nvimtree

---
+++ l-config/nvim/lua/custom/configs/nvimtree.lua
@@ -0,0 +1,13 @@
+local nvimtree_config = require("plugins.configs.nvimtree")
+
+nvimtree_config.renderer.icons.show.git = true
+nvimtree_config.renderer.icons.modified_placement = "before"
+nvimtree_config.renderer.root_folder_label = ":t"
+nvimtree_config.renderer.highlight_git = true
+nvimtree_config.renderer.highlight_opened_files = "all"
+nvimtree_config.renderer.highlight_modified = "all"
+nvimtree_config.modified = {
+	enable = true,
+}
+nvimtree_config.git.enable = true
+nvimtree_config.git.ignore = false
---
+++ l-config/nvim/lua/custom/configs/treesitter.lua
@@ -0,0 +1,23 @@
+local treesitter_config = require("plugins.configs.treesitter")
+
+treesitter_config.ensure_installed = {
+	"vim",
+	"lua",
+	"html",
+	"css",
+	"javascript",
+	"typescript",
+	"tsx",
+	"json",
+	"java",
+	"c",
+	"zig",
+}
+
+treesitter_config.incremental_selection = {
+	enable = true,
+	keymaps = {
+		node_incremental = "v",
+		node_decremental = "V",
+	},
+}
