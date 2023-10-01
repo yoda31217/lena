58e5b666c6f044371e221ee128ed30eb54c9afdc
yoda31217 <jbourne.nik@gmail.com>
2023-10-01 18:43:30 +0300

    Modified files and git highlighting in nvimtree

--- l-config/nvim/lua/custom/plugins.lua
+++ l-config/nvim/lua/custom/plugins.lua
@@ -1,39 +1,36 @@
-local treesitter_config = require "plugins.configs.treesitter"
-treesitter_config.ensure_installed =
-  { "vim", "lua", "html", "css", "javascript", "typescript", "tsx", "json", "java", "c", "zig" }
-treesitter_config.incremental_selection =
-  { enable = true, keymaps = { node_incremental = "v", node_decremental = "V" } }
+require("custom.configs.treesitter")
+require("custom.configs.nvimtree")
 
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
+	{
+		"neovim/nvim-lspconfig",
 
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "custom.configs.null-ls"
-      end,
-    },
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
 
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "html-lsp",
-        "eslint-lsp",
-        "prettier",
-        "stylua",
-        "typescript-language-server",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"html-lsp",
+				"eslint-lsp",
+				"prettier",
+				"stylua",
+				"typescript-language-server",
+			},
+		},
+	},
 }
 
 return plugins
