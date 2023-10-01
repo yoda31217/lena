fe0cfede49ce0edc5e2b990faa0ed014e267ab1f
yoda31217 <jbourne.nik@gmail.com>
2023-10-01 11:26:45 +0300

    Basic scripts skeletons

---
+++ reset-nvim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# Goto current script folder
+cd "$(dirname "$0")"
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
---
+++ setup-nvim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# Goto current script folder
+cd "$(dirname "$0")"
+
+# rm -rf ~/.config/nvim
+# rm -rf ./nvim
+#
+# git clone https://github.com/NvChad/NvChad ./nvim --depth 1
+#
+# ln -s "$(pwd)/nvim" ~/.config/nvim
