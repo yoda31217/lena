4511000257b76bfd3bcad56b747ff054aa432dd8
yoda31217 <jbourne.nik@gmail.com>
2023-10-03 12:00:05 +0300

    remapped hotkeys for horizontal terminal toggle

--- l-config/nvim/lua/custom/mappings.lua
+++ l-config/nvim/lua/custom/mappings.lua
@@ -2,39 +2,55 @@ local M = {}
 
 -- In order to disable a default keymap, use
 M.disabled = {
-  n = {
-    -- ["<leader>h"] = "",
-    -- ["<C-a>"] = "",
-  },
+	n = {
+		-- ["<leader>h"] = "",
+		-- ["<C-a>"] = "",
+	},
 }
 
 -- Your custom mappings
 M.abc = {
-  n = {
-    -- ["<C-n>"] = { "<cmd> Telescope <CR>", "Telescope" },
-    -- ["<C-s>"] = { ":Telescope Files <CR>", "Telescope Files" },
-    ["<C-y>"] = { ":Telescope Files <CR>", "Organize Imports" },
-    ["<leader>fm"] = {
-      function()
-        vim.lsp.buf.format {
-          async = false,
-        }
+	n = {
+		-- ["<C-n>"] = { "<cmd> Telescope <CR>", "Telescope" },
+		-- ["<C-s>"] = { ":Telescope Files <CR>", "Telescope Files" },
+		-- ["<C-h>"] = { "<A-h>", "Lena: Toggle terminal" },
+		["<C-h>"] = {
+			function()
+				require("nvterm.terminal").toggle("horizontal")
+			end,
+			"Toggle horizontal term",
+		},
 
-        vim.lsp.buf.execute_command {
-          command = "_typescript.organizeImports",
-          arguments = { vim.api.nvim_buf_get_name(0) },
-        }
-      end,
-      "Format and organize",
-    },
-  },
+		["<leader>fm"] = {
+			function()
+				vim.lsp.buf.format({
+					async = false,
+				})
 
-  i = {
-    -- ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
-    -- ...
-    -- ["<M-Left>"] = { "<S-Left>", "" },
-    -- ["<M-Right>"] = { "<S-Right>", "" },
-  },
+				vim.lsp.buf.execute_command({
+					command = "_typescript.organizeImports",
+					arguments = { vim.api.nvim_buf_get_name(0) },
+				})
+			end,
+			"Format and organize",
+		},
+	},
+
+	i = {
+		-- ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
+		-- ...
+		-- ["<M-Left>"] = { "<S-Left>", "" },
+		-- ["<M-Right>"] = { "<S-Right>", "" },
+	},
+
+	t = {
+		["<C-h>"] = {
+			function()
+				require("nvterm.terminal").toggle("horizontal")
+			end,
+			"Toggle horizontal term",
+		},
+	},
 }
 
 return M
