6514191d619ea52a6d99ec097b23c3d6f0011613
yoda31217 <jbourne.nik@gmail.com>
2023-10-05 14:45:49 +0300

    Basic Lsp for Java configured

--- l-config/nvim/after/ftplugin/java.lua
+++ l-config/nvim/after/ftplugin/java.lua
@@ -1,80 +1,101 @@
 -- require("core.bootstrap").echo("asdasdasd" .. "123123")
 
 -- vim.notify("This is java file")
---
--- local config = {
--- 	cmd = { "/Users/niknik/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
--- 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
--- }
---
--- require("jdtls").start_or_attach(config)
 
-
--- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
-  -- The command that starts the language server
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-  cmd = {
+	cmd = {
+		"/Users/niknik/.local/share/nvim/mason/packages/jdtls/bin/jdtls",
+		"--jvm-arg=-Dlog.protocol=true -Dlog.level=ALL",
+		-- "-data",
+		-- "/Users/niknik/Documents/projects/thingsteam/best-goods/.lena/nvim/mason/packages/jdtls/data",
+	},
+	root_dir = "/Users/niknik/Documents/projects/thingsteam/best-goods",
 
-    -- 💀
-    'java', -- or '/path/to/java17_or_newer/bin/java'
-            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-
-    -- 💀
-    '-jar', '/path/to/jdtls_install_location/plugins/org.eclipse.equinox.launcher_VERSION_NUMBER.jar',
-         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-         -- Must point to the                                                     Change this to
-         -- eclipse.jdt.ls installation                                           the actual version
-
-
-    -- 💀
-    '-configuration', '/Users/niknik/.local/share/nvim/mason/packages/jdtls/config_mac',
-    -- '-configuration', '/path/to/jdtls_install_location/config_SYSTEM',
-                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
-                    -- eclipse.jdt.ls installation            Depending on your system.
-
-
-    -- 💀
-    -- See `data directory configuration` section in the README
-    '-data', '/Users/niknik/Documents/projects/thingsteam/best-goods'
-    -- '-data', '/path/to/unique/per/project/workspace/folder'
-  },
-
-  -- 💀
-  -- This is the default if not provided, you can remove it. Or adjust as needed.
-  -- One dedicated LSP server & client will be started per unique root_dir
-  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-
-  -- Here you can configure eclipse.jdt.ls specific settings
-  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
-  settings = {
-    java = {
-    }
-  },
-
-  -- Language server `initializationOptions`
-  -- You need to extend the `bundles` with paths to jar files
-  -- if you want to use additional eclipse.jdt.ls plugins.
-  --
-  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-  --
-  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-  init_options = {
-    bundles = {}
-  },
+	settings = {
+		java = {
+			-- home = "/usr/local/jdk-9.0.1",
+			errors = {
+				incompleteClasspath = {
+					severity = "warning",
+				},
+			},
+			eclipse = {
+				downloadSources = true,
+			},
+			configuration = {
+				updateBuildConfiguration = "interactive",
+				maven = {
+					userSettings = nil,
+				},
+			},
+			trace = {
+				server = "verbose",
+			},
+			import = {
+				gradle = {
+					enabled = true,
+				},
+				maven = {
+					enabled = true,
+				},
+				exclusions = {
+					"**/node_modules/**",
+					"**/.metadata/**",
+					"**/archetype-resources/**",
+					"**/META-INF/maven/**",
+					"/**/test/**",
+				},
+			},
+			maven = {
+				downloadSources = true,
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			implementationsCodeLens = {
+				enabled = true,
+			},
+			format = {
+				enabled = true,
+			},
+			saveActions = {
+				organizeImports = false,
+			},
+			contentProvider = {
+				preferred = nil,
+			},
+			autobuild = {
+				enabled = false,
+			},
+			references = {
+				includeDecompiledSources = true,
+			},
+			signatureHelp = { enabled = true },
+			completion = {
+				favoriteStaticMembers = {
+					"org.junit.Assert.*",
+					"org.junit.Assume.*",
+					"org.junit.jupiter.api.Assertions.*",
+					"org.junit.jupiter.api.Assumptions.*",
+					"org.junit.jupiter.api.DynamicContainer.*",
+					"org.junit.jupiter.api.DynamicTest.*",
+				},
+				importOrder = {
+					"java",
+					"javax",
+					"com",
+					"org",
+				},
+			},
+			sources = {
+				organizeImports = {
+					starThreshold = 9999,
+					staticStarThreshold = 9999,
+				},
+			},
+		},
+	},
 }
--- This starts a new client & server,
--- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach(config)
+
+
+require("jdtls").start_or_attach(config)
