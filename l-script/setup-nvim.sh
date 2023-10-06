096399d78a1292948c05a244d8701ebd35339919
yoda31217 <jbourne.nik@gmail.com>
2023-10-06 16:38:49 +0300

    java-debug build added to setup script

--- l-script/setup-nvim.sh
+++ l-script/setup-nvim.sh
@@ -14,6 +14,9 @@ git submodule update
 ln -s "$(pwd)/l-config/nvim/after" l-dependencies/nvchad/after
 ln -s "$(pwd)/l-config/nvim/lua/custom" l-dependencies/nvchad/lua/custom
 
-./l-script/reset-nvim.sh
+cd l-dependencies/microsoft/java-debug
+./mvnw clean install
 
+# We cannot UNDO changes below
+./l-script/reset-nvim.sh
 ln -s "$(pwd)/l-dependencies/nvchad" ~/.config/nvim
