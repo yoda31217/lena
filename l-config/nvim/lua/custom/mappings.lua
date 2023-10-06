557135fb44aa4142c19e91bc4f63577d12dee4c9
yoda31217 <jbourne.nik@gmail.com>
2023-10-06 11:35:41 +0300

    Mappings cleanup

--- l-config/nvim/lua/custom/mappings.lua
+++ l-config/nvim/lua/custom/mappings.lua
@@ -1,74 +1,18 @@
 local M = {}
 
--- In order to disable a default keymap, use
 M.disabled = {
-	n = {
-		-- ["<leader>h"] = "",
-		-- ["<C-a>"] = "",
-	},
+	n = {},
 }
 
--- Your custom mappings
 M.abc = {
 	n = {
 		["<leader>tb"] = { "<Cmd> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
-		-- ["<C-s>"] = { ":Telescope Files <CR>", "Telescope Files" },
-		-- ["<C-h>"] = { "<A-h>", "Lena: Toggle terminal" },
-		-- ["<C-A-h>"] = {
-		-- 	function()
-		-- 		require("nvterm.terminal").toggle("horizontal")
-		-- 	end,
-		-- 	"Toggle horizontal term",
-		-- },
-		--
-		-- ["<leader>fm"] = {
-		-- 	function()
-		-- 		vim.lsp.buf.format({
-		-- 			async = false,
-		-- 		})
-		--
-		-- 		vim.lsp.buf.execute_command({
-		-- 			command = "_typescript.organizeImports",
-		-- 			arguments = { vim.api.nvim_buf_get_name(0) },
-		-- 		})
-		-- 	end,
-		-- 	"Format and organize",
-		-- },
-		-- ["gd"] = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "" },
-		-- ["K"] = { "<Cmd>lua vim.lsp.buf.hover()<CR>", "" },
-		-- ["rn"] = { "<cmd>lua vim.lsp.buf.rename()<CR>", "" },
-		-- ["gr"] = { "<cmd>lua vim.lsp.buf.references()<CR>", "" },
-		-- ["<leader>ld"] = { "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", "" },
-		-- ["[d"] = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", "" },
-		-- ["]d"] = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "" },
-		-- ["<leader>q"] = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", "" },
-
-		-- My hotkeys
 		["<leader>tw"] = { "<CMD>lua vim.o.wrap = not vim.o.wrap<CR>", "Toggle word wrapping in the current buffer" },
 	},
 
-	i = {
-		-- ["<C-M-h>"] = {
-		-- 	function()
-		-- 		require("nvterm.terminal").toggle("horizontal")
-		-- 	end,
-		-- 	"Toggle horizontal term",
-		-- },
+	i = {},
 
-		-- ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
-		-- ...
-		-- ["<M-Left>"] = { "<S-Left>", "" },
-		-- ["<M-Right>"] = { "<S-Right>", "" },
-	},
-
-	t = {
-		-- ["<C-M-h>"] = {
-		-- 	function()
-		-- 		require("nvterm.terminal").toggle("horizontal")
-		-- 	end,
-		-- 	"Toggle horizontal term",
-		-- },
-	},
+	t = {},
 }
 
 return M
