b02b313e54f6f976ab46d33adcf344809322e734
yoda31217 <jbourne.nik@gmail.com>
2023-10-07 08:20:09 +0300

    Lua formatting fixed

--- l-config/nvim/lua/custom/configs/null-ls.lua
+++ l-config/nvim/lua/custom/configs/null-ls.lua
@@ -6,15 +6,19 @@ local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 local sources = {
-   formatting.prettier.with ({extra_args={"--single-quote", "--print-width", "120"}}),
-   formatting.stylua,
+  formatting.prettier.with { extra_args = { "--single-quote", "--print-width", "120" } },
+  formatting.stylua.with {
+    extra_args = {
+      "--config-path",
+      "~/.config/nvim/.stylua.toml",
+    },
+  },
 
-   -- lint.shellcheck,
-   -- lint.eslint,
+  -- lint.shellcheck,
+  -- lint.eslint,
 }
 
 null_ls.setup {
-   debug = true,
-   sources = sources,
+  debug = true,
+  sources = sources,
 }
-
