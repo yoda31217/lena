05c1c7069aec834d5af04d5a2ddd69ddeff5acf1
yoda31217 <jbourne.nik@gmail.com>
2023-10-07 12:21:14 +0300

    Relative line number switch keymapping added

--- l-config/nvim/lua/custom/mappings.lua
+++ l-config/nvim/lua/custom/mappings.lua
@@ -1,19 +1,23 @@
 local M = {}
 
 M.disabled = {
-	n = {},
+  n = {},
 }
 
 M.abc = {
-	n = {
-		["<leader>tb"] = { "<CMD> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
-		["<leader>tw"] = { "<CMD>lua vim.o.wrap = not vim.o.wrap<CR>", "Toggle word wrapping in the current buffer" },
-		["<CR>"] = { "a<CR><ESC>", "Append New Line in Normal mode" },
-	},
+  n = {
+    ["<leader>tb"] = { "<CMD> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
+    ["<leader>tw"] = { "<CMD>lua vim.o.wrap = not vim.o.wrap<CR>", "Toggle word wrapping in the current buffer" },
+    ["<leader>rn"] = {
+      "<CMD>lua vim.o.relativenumber = not vim.o.relativenumber<CR>",
+      "Toggle relatine line number in the current buffer",
+    },
+    ["<CR>"] = { "a<CR><ESC>", "Append New Line in Normal mode" },
+  },
 
-	i = {},
+  i = {},
 
-	t = {},
+  t = {},
 }
 
 return M
