b8fba5c09f79fad3bef1a9c48e260795665ec6ae
yoda31217 <jbourne.nik@gmail.com>
2023-10-07 10:48:08 +0300

    Normal mode <CR> mapping

--- l-config/nvim/lua/custom/mappings.lua
+++ l-config/nvim/lua/custom/mappings.lua
@@ -6,8 +6,9 @@ M.disabled = {
 
 M.abc = {
 	n = {
-		["<leader>tb"] = { "<Cmd> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
+		["<leader>tb"] = { "<CMD> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
 		["<leader>tw"] = { "<CMD>lua vim.o.wrap = not vim.o.wrap<CR>", "Toggle word wrapping in the current buffer" },
+		["<CR>"] = { "a<CR><ESC>", "Append New Line in Normal mode" },
 	},
 
 	i = {},
