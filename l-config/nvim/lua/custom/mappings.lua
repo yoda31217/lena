84d8b2508287ce823ead01f6d5071715f5b33b42
yoda31217 <jbourne.nik@gmail.com>
2023-10-07 15:29:05 +0300

    General mappings impruvement

--- l-config/nvim/lua/custom/mappings.lua
+++ l-config/nvim/lua/custom/mappings.lua
@@ -6,13 +6,13 @@ M.disabled = {
 
 M.abc = {
   n = {
-    ["<leader>tb"] = { "<CMD> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
-    ["<leader>tw"] = { "<CMD>lua vim.o.wrap = not vim.o.wrap<CR>", "Toggle word wrapping in the current buffer" },
-    ["<leader>rn"] = {
+    ["<C-S-t>"] = { "<CMD> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
+    ["<C-S-w>"] = { "<CMD>lua vim.o.wrap = not vim.o.wrap<CR>", "Toggle word wrapping in the current buffer" },
+    ["<C-S-n>"] = {
       "<CMD>lua vim.o.relativenumber = not vim.o.relativenumber<CR>",
       "Toggle relatine line number in the current buffer",
     },
-    ["<CR>"] = { "a<CR><ESC>", "Append New Line in Normal mode" },
+    ["<CR>"] = { "a<CR><ESC><RIGHT>", "Append New Line in Normal mode" },
   },
 
   i = {},
