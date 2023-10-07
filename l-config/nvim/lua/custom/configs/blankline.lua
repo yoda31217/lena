b92881c0c9977f499a6144cd1447315c936a0f70
yoda31217 <jbourne.nik@gmail.com>
2023-10-07 09:13:43 +0300

    Removed not needed garbage blank characters

--- l-config/nvim/lua/custom/configs/blankline.lua
+++ l-config/nvim/lua/custom/configs/blankline.lua
@@ -1,9 +1,12 @@
 local blankline_config = require("plugins.configs.others").blankline
 
-vim.opt.list = true
-vim.opt.listchars:append("space:•")
-vim.opt.listchars:append("eol:󱞣")
+-- vim.opt.list = true
+-- vim.opt.listchars:append("space:•")
+-- vim.opt.listchars:append("eol:󱞣")
+--
+-- blankline_config.show_end_of_line = true
+-- blankline_config.space_char_blankline = " "
 
-blankline_config.show_end_of_line = true
-blankline_config.space_char_blankline = " "
-
+blankline_config.enabled = 0
+blankline_config.show_current_context = false
+blankline_config.show_current_context_start = false
