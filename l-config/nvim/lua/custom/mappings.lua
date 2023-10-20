68902d15bb9b55cc3aee8ccdecea6535d87aa132
yoda31217 <jbourne.nik@gmail.com>
2023-10-20 23:17:01 +0300

    Fixed quickfix files jumping

--- l-config/nvim/lua/custom/mappings.lua
+++ l-config/nvim/lua/custom/mappings.lua
@@ -9,7 +9,7 @@ M.abc = {
     ["<C-S-l>"] = { "<CMD>LspRestart<CR>", "Restart LSP" },
     ["<C-S-t>"] = { "<CMD> Telescope builtin<CR>", "Lists of Built-in Telescope pickers" },
     ["<C-S-w>"] = { "<CMD>lua vim.o.wrap = not vim.o.wrap<CR>", "Toggle word wrapping in the current buffer" },
-    ["<CR>"] = { "a<CR><ESC><RIGHT>", "Append New Line in Normal mode" },
+    -- ["<CR>"] = { "a<CR><ESC><RIGHT>", "Append New Line in Normal mode" },
     ["<C-S-h>"] = {
       function()
         require("nvterm.terminal").toggle("horizontal")
