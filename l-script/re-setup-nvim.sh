156de65405dee07d7481e1a9197643f9d8bb43b2
yoda31217 <jbourne.nik@gmail.com>
2023-11-11 16:24:12 +0200

    Re setup shell script added

--- l-script/re-setup-nvim.sh
+++ l-script/re-setup-nvim.sh
@@ -9,4 +9,7 @@ cd "$(dirname "$0")"
 cd ..
 find . -name . -o -prune -exec rm -rf -- {} +
 
-git clone https://github.com/yoda31217/lena.git . && lena/l-script/setup-nvim.sh
+git clone https://github.com/yoda31217/lena.git .
+
+l-script/setup-nvim.sh
+
