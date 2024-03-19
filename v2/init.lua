41d2b7b417bc660c3fde8bc3db2fecd600a45144
yoda31217 <jbourne.nik@gmail.com>
2024-03-19 06:20:55 +0200

    Colorscheme improved

--- v2/init.lua
+++ v2/init.lua
@@ -812,7 +812,7 @@ require("lazy").setup({
       vim.cmd.colorscheme("tokyonight-night")
 
       -- You can configure highlights by doing something like
-      vim.cmd.hi("Comment gui=none")
+      -- vim.cmd.hi("Comment gui=none")
     end,
   },
 
