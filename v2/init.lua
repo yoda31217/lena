d61796d67b10d9793c4d237e25009c27b46c3ee7
yoda31217 <jbourne.nik@gmail.com>
2024-03-22 15:25:03 +0200

    Statusline location section configured

--- v2/init.lua
+++ v2/init.lua
@@ -866,9 +866,9 @@ require("lazy").setup({
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return "%2l:%-2v"
-      end
+      -- statusline.section_location = function()
+      --   return "%2l:%-2v"
+      -- end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
