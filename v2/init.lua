5930aa4642a62d422bfd9d5bd7836ea3af600fec
yoda31217 <jbourne.nik@gmail.com>
2024-03-22 15:24:16 +0200

    Recent buffer selection improved

--- v2/init.lua
+++ v2/init.lua
@@ -403,7 +403,14 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set("n", "<leader><leader>", function()
-        builtin.buffers({ sort_mru = true })
+        builtin.buffers({
+          sort_mru = true,
+          on_complete = {
+            function()
+              vim.cmd("stopinsert")
+            end,
+          },
+        })
       end, { desc = "[ ] Find existing buffers" })
 
       -- Slightly advanced example of overriding default behavior and theme
