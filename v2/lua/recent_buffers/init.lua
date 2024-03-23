074a01fd71e7a5531620a1fa60b203ffc7df6388
yoda31217 <jbourne.nik@gmail.com>
2024-03-23 03:36:21 +0200

    Recent buffers file path view improved

--- v2/lua/recent_buffers/init.lua
+++ v2/lua/recent_buffers/init.lua
@@ -22,25 +22,24 @@ local displayer = pickers_entry_display.create({
 local make_display = function(entry)
   return displayer({
     { entry.index - 1, "TelescopeResultsNumber" },
-    { entry.filename, "TelescopeMatching" },
+    { entry.filename_tail, "TelescopeMatching" },
     entry.directory,
   })
 end
 
 local function entry_maker(entry)
-  local absolute_file_path = entry.info.name ~= "" and entry.info.name
-    or "[No Name]"
-  local filename = utils.path_tail(absolute_file_path)
-  local absolute_directory_path = Path:new(absolute_file_path):parent()
-  local relative_directory_path = Path:new(absolute_directory_path)
-    :normalize(cwd)
+  local filename = entry.info.name ~= "" and entry.info.name or "[No Name]"
+  local filename_tail = utils.path_tail(filename)
+  local absolute_directory = Path:new(filename):parent()
+  local relative_directory = Path:new(absolute_directory):normalize(cwd)
 
   return make_entry.set_default_entry_mt({
     value = entry,
-    ordinal = absolute_file_path,
+    ordinal = filename,
     display = make_display,
     filename = filename,
-    directory = relative_directory_path,
+    filename_tail = filename_tail,
+    directory = relative_directory,
     bufnr = entry.bufnr,
   }, {})
 end
@@ -54,25 +53,8 @@ local function select_buffer_by_index(target_buffer_index, prompt_bufnr)
 end
 
 local function delete_selected_buffer(prompt_bufnr)
-  -- local selected_buffer_entry = actions_state.get_selected_entry()
-  -- local selected_buffer_index = selected_buffer_entry.index
-  -- local target_buffer_offset = selected_buffer_index
-  --   - target_buffer_index
-  -- action_set.shift_selection(prompt_bufnr, target_buffer_offset)
-  -- actions.select_default(prompt_bufnr)
-  -- action_set.edit(prompt_bufnr, "drop")
-
   actions.delete_buffer(prompt_bufnr)
   actions.close(prompt_bufnr)
-  -- vim.cmd("<leader><leader>")
-  -- local current_picker =
-  --   actions_state.get_current_picker(prompt_bufnr)
-  -- current_picker:delete_selection(function(selection)
-  --   -- local force = vim.api.nvim_buf_get_option(selection.bufnr, "buftype") == "terminal"
-  --   -- local ok = pcall(vim.api.nvim_buf_delete, selection.bufnr, { force = force })
-  --   -- return ok
-  --   return true
-  -- end)
 end
 
 local function show_recent_buffers()
