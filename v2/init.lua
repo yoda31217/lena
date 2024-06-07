b68f475f80320b65ded81bb84aff6da9cdc6b0bc
yoda31217 <jbourne.nik@gmail.com>
2024-06-07 15:13:18 +0300

    Signatore help keymappings added on function call adit

--- v2/init.lua
+++ v2/init.lua
@@ -686,6 +686,12 @@ require("lazy").setup({
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap
           map("K", vim.lsp.buf.hover, "Hover Documentation")
+          vim.keymap.set(
+            "i",
+            "<C-s>",
+            vim.lsp.buf.signature_help,
+            { buffer = event.buf, desc = "LSP: [S]ignature Help" }
+          )
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header
